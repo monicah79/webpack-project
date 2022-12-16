aabca38dc307e339db8126f92f9ebde279e76cd3
monicah79 <monniewanjema@gmail.com>
2022-12-16 14:17:59 +0300

    To do list structure setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,46 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "production",
-    entry:{
-    index: path.resolve(__dirname, './src/index.js')
-},
-devServer: {
+  mode: 'production',
+  entry: {
+    index: path.resolve(__dirname, './src/index.js'),
+  },
+  devServer: {
     static: './dist',
   },
 
-output: {
-    path : path.resolve(__dirname, './dist'),
-    filename:'[name].bundle.js'
-},
-plugins: [
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/index.html")
+      template: path.resolve(__dirname, './src/index.html'),
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
       },
     ],
   },
+ 
   optimization: {
     runtimeChunk: 'single',
   },
-}
+};
